57ef3cdeaab84e799b2c5c5607008869df3c9386
Adriel Gama <adrielgama@gmail.com>
2018-03-14 15:13:27 -0300

    Update 078.c

--- c/lista100/078.c
+++ c/lista100/078.c
@@ -1,15 +1,21 @@
-/* Escreva um programa em C, que leia uma string e um caracter e conte o número de
-ocorrências do caracter lido na string. */
+/* Escreva um programa em C, que leia uma string e um caracter e conte o nÃºmero de
+ocorrÃªncias do caracter lido na string. */
 #include <stdio.h>
 int main(){
 	char caracter;
 	char string[30];
 	int total = 0;
+	
 	fgets(string, 30, stdin);
+	
+	scanf("%c", &caracter);
+	
 	for(int i = 0; i<30; i++){
 		if(string[i]==caracter)
 		    total += 1;
 	}
+	
 	printf("%d\n", total);
+	
 	return 0;
 }
